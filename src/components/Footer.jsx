f29cc8ff71e041e579a93ecd9132431e3c201511
Laraib Anwar <laraibanwar11@gmail.com>
2026-05-01 13:21:19 +0530

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full bg-black border-t border-[#1e1e1e] h-[60px] flex items-center justify-center">
       <p className="font-dm text-[#555] text-xs">
-        © 2025 LAFlix. All rights reserved. Created by Laraib Anwar.
+        © 2026 LAFlix. All rights reserved. Created by Laraib Anwar.
       </p>
     </footer>
   );
